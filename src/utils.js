e7f1a6c118d733a921a346157ac0e1ecc6dce772
Sanish <sanishkr@gmail.com>
2021-07-20 19:20:13 +0000

    :alembic: testing if publish works

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
-function randomColor(colors) {
-  return colors[random(0, colors.length)];
-}
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function randomColor(colors) {
+  if(Array.isArray(colors) && colors.length) {
+    return colors[random(0, colors.length)];
+  } else {
+    return null
+  }
 }
 
 export {
