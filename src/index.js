4b974f40b0bb4d21a5612ee34e8cdb8729c62453
Sanish <sanishkr@gmail.com>
2022-02-27 13:55:36 +0000

    :sparkles: Add burst animation and pop sound

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import mojs from "@mojs/core";
+
 import {random, randomColor} from './utils'
 
 import './index.css';
@@ -13,6 +15,7 @@ const ReactFloatingBalloons = ({
 }) => {
 	React.useEffect(() => {
         // console.log({count, loop, name});
+		bdayBallons(count, [], colors);
 		bdayBallons();
 	}, []);
 	return null;
@@ -20,20 +23,58 @@ const ReactFloatingBalloons = ({
 
 export { ReactFloatingBalloons };
 
+// const bdayBallons = function ({density, balloons = [], colors}) {
 const bdayBallons = function () {
-	const density = 7; // concurrent balloon count
+	const density = 5; // concurrent balloon count
 	const balloons = [];
 	const colors = ['yellow', 'green', 'blue', 'red'];
+	const msgText = 'Happy Birthday.'
+	let audio = new Audio('https://soundbible.com/mp3/Balloon%20Popping-SoundBible.com-1247261379.mp3');
+	audio.volume = 0.2;
 
 	const stringElement = document.createElement('div');
 	stringElement.classList.add('string');
+	const msgElement = document.createElement('span');
+	msgElement.innerText = msgText;
+	msgElement.classList.add('msg');
+
+	const popBalloon = (e) => {
+		let t = e.currentTarget;
+		const burst = new mojs.Burst({
+			radius: { 30: 100 },
+			parent: t,
+			count: 10,
+			className: 'show',
+			children: {
+				fill: [t.classList[2]],
+				angle: { 0: 180 },
+				delay: 'stagger(0, 25)',
+				shape: [ 'circle', 'polygon' ],
+			}
+		});
+		if(t.classList.contains('balloon')) {
+			audio.play();
+			t.style.visibility = 'hidden';
+			// burst.style.visibility = 'visible'
+			// t.classList.toggle('show');
+			// t.append(burst.cloneNode())
+			// console.log(t.classList)
+		} else {
+			audio.play();
+			t.parentElement.classList.toggle('show');
+			console.log(t.parentElement.classList, '------')
+		}
+		burst.replay();
+	};
 
 	for (let i = 0; i < density; i++) {
 		const element = document.createElement('div');
-		element.classList.add('balloon');
-		element.classList.add(randomColor(colors));
+		element.classList.add('balloon', 'show');
+		element.classList.add(randomColor(colors))
+		element.addEventListener('dblclick', popBalloon);
 
 		element.append(stringElement.cloneNode());
+		element.append(msgElement.cloneNode(random(0,2)===0));
 		document.body.append(element);
 
 		setTimeout(() => {
@@ -43,8 +84,10 @@ const bdayBallons = function () {
 
 	function releaseBalloon(balloon) {
 		const delay = random(100, 1000);
-		const x = random(-59, -10); // random x value to fly
-		const y = random(-99, 0); // random y value to fly
+		const x = random(-99, -30); // random x value to fly
+    const y = random(-99, -30); // random y value to fly
+		// const x = random(-59, -10); // random x value to fly
+		// const y = random(-99, 0); // random y value to fly
 
 		const sequence = [
 			{
@@ -100,6 +143,6 @@ const bdayBallons = function () {
 			delay: delay,
 		});
 
-		// balloonAnimation.onfinish = () => { releaseBalloon(balloon) }
+		balloonAnimation.onfinish = () => { releaseBalloon(balloon) }
 	}
 };
